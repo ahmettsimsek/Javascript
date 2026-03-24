dfc2102ae80d6d29ca51b633b5b44960d5ccde95
ahmettsimsek <ahmtsmsek7@gmail.com>
2026-03-24 20:51:57 +0300

    Gün 2

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const text = document.getElementById('text')
+/*const text = document.getElementById('text')
 console.log(text, 'xfgdsfgdg')
 
 text.innerText = "yeni mesaj " // HTML sayfadakı textimizi değiştirir
+*/
 
-
+/*
 const btn = document.getElementById('btn')
-btn.addEventListener('click', () => {
+btn.addEventListener('click', () => { // click eklediğim için listener zorunlu eklemek
     text.innerText = "yeni mesaj " // butona tıklama olunca texti o zaman değiştir deriz
 })
+*/
 
-const btn2 = document.getElementById('btn2')
+/*
+const btn = document.getElementById('btn')
+let count = 0
+btn.addEventListener('click', () => { // click eklediğim için listener zorunlu eklemek
+    text.innerText = count + 1// butona tıklama olunca texti o zaman değiştir deriz
+    return count
+})
+*/
+
+/*
+const btn = document.getElementById('btn')
+btn.addEventListener('click', () => { // click eklediğim için listener zorunlu eklemek
+    prompt('deneme promptu')
+})
+*/
+
+const funcOne = () => {
+    prompt('deneme mesajı')
+}   
